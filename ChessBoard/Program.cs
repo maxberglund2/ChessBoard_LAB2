023784ee579e05aa51de435a8f84574f6ef5af80
Greed <tf2bro999@gmail.com>
2024-09-11 09:21:29 +0200

    Add project files.

---
+++ ChessBoard/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ChessBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in en siffra:");
+
+            int userNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < userNumber; i++)
+            {
+                for (int j = 0; j < userNumber; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write("O");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
